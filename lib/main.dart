6a5a4994318d9dafebd6b4e34ee260b16df21484
piro-piyush <piyush72717272@gmail.com>
2024-10-09 15:34:59 +0530

    update home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:chat_app/pages/sign_in_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/pages/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'pages/home_screen/home.dart';
 import 'services/auth_methods.dart';
 
